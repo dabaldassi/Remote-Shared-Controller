5a3a1035c78259062860fb4a471199d02b079d39
Papin <alexpapin68@gmail.com>
2019-12-18 17:11:25 +0100

    :pencil: [network] Add documentation for interface.h

--- src/network/interface.h
+++ src/network/interface.h
@@ -7,9 +7,26 @@ extern "C" {
 
 #include <net/if.h>
 
+  /**
+   * @brief Structure that contain the name of an interface and its index.
+   * Alias for struct if_nameindex in net/if.h
+   */
+
   typedef struct if_nameindex IF;
 
-  IF *  get_interfaces();
+  /**
+   * @brief Returns the name and the index of all interfaces.
+   * Alias for if_nameindex() in net/if.h
+   * @return Array of name and index of interfaces.
+   */
+
+  IF * get_interfaces();
+
+  /**
+   * @brief Frees an IF array.
+   * Alias for if_freenameindex() in net/if.h
+   * @param interfaces IF array.
+   */
 
   void free_interfaces(IF * interfaces);
 

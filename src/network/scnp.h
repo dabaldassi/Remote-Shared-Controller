3ad27e88c6e2aeb92e80edb432697a34643e6b58
Alexandre Papin <alexpapin68@gmail.com>
2020-02-09 17:36:42 +0100

    :pencil: [network] Add documentation for SCNP packet structure

--- src/network/scnp.h
+++ src/network/scnp.h
@@ -8,65 +8,152 @@ extern "C" {
 #include <stdint.h>
 #include <stdbool.h>
 
-
+/* EtherType */
 #define ETH_P_SCNP 0x8888
 
+/* Types */
 #define SCNP_KEY 0x01
 #define SCNP_MOV 0x02
 #define SCNP_OUT 0x03
 #define SCNP_MNG 0xfe
 #define SCNP_ACK 0xff
 
+/* Type lengths */
 #define KEY_LENGTH 8
 #define MOV_LENGTH 7
 #define OUT_LENGTH 8
 #define MNG_LENGTH 65
 #define ACK_LENGTH 5
 
+/* Maximum length of SCNP packet */
 #define MAX_PACKET_LENGTH 127
+
+/* Hostname length in SCNP management */
 #define HOSTNAME_LENGTH 64
+
+/* SCNP out flags */
 #define OUT_EGRESS true
 #define OUT_INGRESS false
 #define OUT_RIGHT true
 #define OUT_LEFT false
 
 
+/**
+ * @struct struct scnp_packet
+ * @brief SCNP packet generic structure.
+ *
+ * Structure of a SCNP packet used for parameters of every SCNP methods.
+ *
+ * @var type Type of the SCNP packet.
+ * @var data Data contained by the SCNP packet.
+ */
+
 struct scnp_packet
 {
-  uint8_t type;                          // Type of the SCNP packet
-  uint8_t data[MAX_PACKET_LENGTH - 1];   // Data in the SCNP packet
+  uint8_t type;
+  uint8_t data[MAX_PACKET_LENGTH - 1];
 };
+
+/**
+ * @struct struct scnp_key
+ * @brief SCNP key structure.
+ *
+ * Structure of a SCNP packet used to send a key event.
+ *
+ * @var type Type of the SCNP packet. Must be SCNP_KEY.
+ * @var id Identifier of the SCNP packet. The corresponding acknowledgement
+ * need the same identifier. The library set this field automatically,
+ * it may be set to zero.
+ * @var code Code associated with the key event.
+ * @var pressed Equals to true if the key is pressed, false otherwise.
+ * @var repeated Equals to true if the key is repeated, false otherwise.
+ */
 
 struct scnp_key
 {
-  uint8_t type;   // SCNP_KEY
-  uint32_t id;    // identifier
-  uint16_t code;  // Code associated to the key event
-  bool pressed;   // true if the key is pressed, false otherwise (even if repeated is true)
-  bool repeated;  // true if the key is repeated, false otherwise
+  uint8_t type;
+  uint32_t id;
+  uint16_t code;
+  bool pressed;
+  bool repeated;
 };
+
+/**
+ * @struct struct scnp_movement
+ * @brief SCNP movement structure.
+ *
+ * Structure of a SCNP packet used to send a movement event.
+ *
+ * @var type Type of the SCNP packet. Must be SCNP_MOV.
+ * @var code Code associated with the movement event.
+ * @var value Value of the movement.
+ */
 
 struct scnp_movement
 {
-  uint8_t type;     // SCNP_MOV
-  uint16_t code;    // Code associated to the movement event
-  int32_t value;    // Value of the movement
+  uint8_t type;
+  uint16_t code;
+  int32_t value;
 };
+
+/**
+ * @struct struct scnp_out
+ * @brief SCNP out structure.
+ *
+ * Structure of a SCNP packet used to indicate that the cursor has exited
+ * or entered a screen.
+ *
+ * @var type Type of the SCNP packet. Must be SCNP_OUT.
+ * @var id Identifier of the SCNP packet. The corresponding acknowledgement
+ * need the same identifier. The library set this field automatically,
+ * it may be set to zero.
+ * @var direction Direction of screen change. Equals to true (OUT_EGRESS)
+ * if the cursor is exiting a screen, false (OUT_INGRESS) if the mouse is
+ * entering a screen.
+ * @var side Side of the screen where the cursor is exiting or entering.
+ * Equals to true (OUT_RIGHT) for the right side, false (OUT_LEFT) for the left
+ * side.
+ * @var height Height of the cursor relative to the size of the screen. Must be
+ * between zero and one.
+ */
 
 struct scnp_out
 {
-  uint8_t type;         // SCNP_OUT
-  uint32_t id;          // identifier
-  bool direction;       // true if the mouse is going from the host to the client, false otherwise
-  bool side;            // true if the mouse is located at the right border of the screen, false if it is on the left
-  float height;         // Height of the cursor relative to the size of the screen
+  uint8_t type;
+  uint32_t id;
+  bool direction;
+  bool side;
+  float height;
 };
+
+/**
+ * @struct struct scnp_management
+ * @brief SCNP management structure.
+ *
+ * Structure of a SCNP packet used to indicate the existence of the device.
+ *
+ * @var type Type of the SCNP packet. Must be SCNP_MNG.
+ * @var hostname Hostname of the device. Its length must not exceed
+ * 63 characters. The character following the last one must be '\0'.
+ */
 
 struct scnp_management
 {
-  uint8_t type;                     // SCNP_MNG
+  uint8_t type;
   char hostname[HOSTNAME_LENGTH];   // Hostname label of the device linked with the source interface
 };
+
+/**
+ * @struct scnp_ack
+ * @brief SCNP acknowledgement structure.
+ *
+ * Structure of a SCNP packet used to confirm the reception of another
+ * SCNP packet.
+ *
+ * @var type Type of the SCNP packet. Must be SCNP_ACK.
+ * @var id Identifier of the SCNP packet. Must equals to the identifier of
+ * the received packet that needs acknowledgement.
+ */
 
 struct scnp_ack
 {

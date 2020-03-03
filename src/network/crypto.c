aa859c6bee6bdd09aaaca9a89499056a2b9ade0c
dabaldassi <david.baldassin@etu.uca.fr>
2020-03-03 22:28:13 +0100

    :checkered_flag: Fix build crypto on Windows

--- src/network/crypto.c
+++ src/network/crypto.c
@@ -6,7 +6,9 @@
 int encrypt(uint8_t * payload, unsigned int plength, const uint8_t * key, unsigned int klength)
 {
   if (klength < plength) {
-    errno = EKEYREJECTED;
+#ifndef _WIN32
+      errno = EKEYREJECTED;
+#endif // !_WIN32
     return -1;
   }
   for (unsigned int i = 0u; i < plength; ++i) {
